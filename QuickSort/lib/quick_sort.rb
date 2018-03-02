47edba7c87ef909be8b024d9c83779ee93d65424
Emmanuel <emmanuel.feygelson@gmail.com>
2018-03-02 00:29:59 -0800

    Delete folder, fix quicksort

--- QuickSort/lib/quick_sort.rb
+++ QuickSort/lib/quick_sort.rb
@@ -22,33 +22,28 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    return array if array.length <= 2
-
+    return array if length <= 1
+    pivot = partition(array, start, length, &prc)
+    sort2!(array, start, pivot - start, &prc)
+    sort2!(array, pivot + 1, length - 1 - pivot - start, &prc)
   end
 
   def self.partition(array, start, length, &prc)
     prc ||= Proc.new { |x, y| x <=> y }
+    bound = start
 
-    par = start + 1
-    
-  end
-end
-
-def blaa(array)
-  return array if array.length <= 1
-  pivot = array[0]
-  i = 1
-  left = []
-  right = []
-  while i < array.length
-    if array[i] <= pivot
-      left << array[i]
-    else
-      right << array[i]
+    ((start + 1)..(start + length - 1)).each do |i|
+      if prc.call(array[start], array[i]) > 0
+        bound += 1
+        array[bound], array[i] = array[i], array[bound]
+      end
     end
-    i += 1
+
+    array[bound], array[start] = array[start], array[bound]
+    bound
   end
-  blaa(left) + [pivot] + blaa(right)
 end
+
 
 # p QuickSort.sort1([5, 9, 1, 19, 3, 99, 23, 4, 5, 17, 0])
+# p QuickSort.sort2!([5, 9, 1, 19, 3, 99, 23, 4, 5, 17, 0])
